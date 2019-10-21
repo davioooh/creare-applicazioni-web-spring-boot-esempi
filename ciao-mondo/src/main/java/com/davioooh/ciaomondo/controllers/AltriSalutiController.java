9e7f32bd2d7fb01c112fe256b900e17f68d001ca
David Castelletti <d.castelletti@cogitoo.com>
2019-10-21 12:41:38 +0200

    Aggiunto esempio con annotazione @ModelAttribute

---
+++ ciao-mondo/src/main/java/com/davioooh/ciaomondo/controllers/AltriSalutiController.java
@@ -0,0 +1,26 @@
+package com.davioooh.ciaomondo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/altri-saluti")
+public class AltriSalutiController {
+
+    @GetMapping
+    public String unSalutoSemplice() {
+        return "ciao-mondo";
+    }
+
+    @GetMapping("/verboso")
+    public String unSalutoVerboso() {
+        return "ciao-mondo-verboso";
+    }
+
+    @ModelAttribute("nome")
+    public String nome() {
+        return "David";
+    }
+}
