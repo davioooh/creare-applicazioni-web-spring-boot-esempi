40cece53c6a7e4068a04fe38b74d5839eabd531b
David Castelletti <d.castelletti@cogitoo.com>
2019-10-21 11:15:27 +0200

    Aggiunto model attribute a metodo unSaluto

--- ciao-mondo/src/main/java/com/davioooh/ciaomondo/controllers/SalutiController.java
+++ ciao-mondo/src/main/java/com/davioooh/ciaomondo/controllers/SalutiController.java
@@ -3,6 +3,7 @@ package com.davioooh.ciaomondo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -11,8 +12,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class SalutiController {
 
     @RequestMapping(method = GET)
-    public String unSaluto() {
-        return "ciao-mondo";
+    public ModelAndView unSaluto() {
+        return new ModelAndView("ciao-mondo")
+                .addObject("nome", "David");
     }
 
     @GetMapping(params = "mio-parametro")
