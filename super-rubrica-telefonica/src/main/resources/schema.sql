f16ff0cf937ea2ac525b306e13ae2998b32598e6
David Castelletti <d.castelletti@cogitoo.com>
2020-05-12 10:30:54 +0200

    Aggiunti ruoli utente

--- super-rubrica-telefonica/src/main/resources/schema.sql
+++ super-rubrica-telefonica/src/main/resources/schema.sql
@@ -13,7 +13,24 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 INSERT INTO users (id, username, password)
- VALUES (1, 'testuser', '{noop}pwd')
+ VALUES
+    (1, 'testuser', '{noop}pwd'),
+    (2, 'testuser2', '{noop}pwd'),
+    (3, 'adminuser', '{noop}admin')
+ ON CONFLICT DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS user_roles (
+    user_id int,
+    role varchar(20),
+    PRIMARY KEY (user_id, role)
+);
+
+INSERT INTO user_roles (user_id, role)
+ VALUES
+    (1, 'USER'),
+    (2, 'USER'),
+    (3, 'USER'),
+    (3, 'ADMIN')
  ON CONFLICT DO NOTHING;
 
 ALTER TABLE contacts ADD COLUMN IF NOT EXISTS owner_username varchar(100) NOT NULL DEFAULT 'testuser';
