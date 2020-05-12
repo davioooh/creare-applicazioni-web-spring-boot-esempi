ddf17d38f7ca059dd370d2c2efe4ec00e8bb3f98
David Castelletti <d.castelletti@cogitoo.com>
2020-05-12 10:55:43 +0200

    Redirect da index a lista contatti

--- super-rubrica-telefonica/src/main/java/com/davioooh/superrubrica/index/IndexController.java
+++ super-rubrica-telefonica/src/main/java/com/davioooh/superrubrica/index/IndexController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 class IndexController {
     @GetMapping("/")
     String indexPage() {
-        return "index";
+        return "redirect:/contacts";
     }
 }
